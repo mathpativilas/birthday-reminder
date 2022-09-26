7317b0ba9484e4487a9364aba65e38fc571066f3
vilas <atechy.vilas@gmail.com>
2022-09-26 23:24:52 +0400

    final

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import List from "./components/List";
-// import data from "./components/data";
+import data from "./components/data";
 function App() {
-  console.log("hello");
+  const [people, setPeople] = useState(data);
+
+  const clickHandler = () => {
+    setPeople([]);
+  };
+  const AddHandler = () => {
+    setPeople(data);
+  };
   return (
-    <>
-      <List />
-    </>
+    <main>
+      <section className="container">
+        <h3> {people.length} Peoples Birthday today</h3>
+        <List people={people} />
+        <button onClick={clickHandler}>Clear All</button>
+        <button onClick={AddHandler}>Add</button>
+      </section>
+    </main>
   );
 }
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-const List = () => {
+const List = ({ people }) => {
   return (
     <>
-      <h2>Lists</h2>
+      {people.map((item) => {
+        const { id, name, image, age } = item;
+        return (
+          <article key={id} className="person">
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        );
+      })}
     </>
   );
 };
